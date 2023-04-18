d16ff185e70f326dadf799b098a2554bb28dd4bf
Shafikov Artur <lubetsiky02@mail.ru>
2023-04-18 19:28:37 +0500

    Fixed parsers tests

--- project/link-parser/src/main/test/ru/tinkoff/edu/java/LinkParser/Parsers/StackLinkParserTest.java
+++ project/link-parser/src/main/test/ru/tinkoff/edu/java/LinkParser/Parsers/StackLinkParserTest.java
@@ -2,28 +2,29 @@ package ru.tinkoff.edu.java.LinkParser.Parsers;
 
 import org.junit.Before;
 import org.junit.Test;
-import ru.tinkoff.edu.java.LinkParser.LinksInfo.LinkInfo;
+import ru.tinkoff.edu.java.LinkParser.data.LinkData;
+import ru.tinkoff.edu.java.LinkParser.parsers.StackOverFlowLinkParser;
 
 import static org.junit.Assert.*;
 
 public class StackLinkParserTest {
 
-    StackLinkParser parser;
+    StackOverFlowLinkParser parser;
     @Before
     public void setUp() throws Exception {
-        parser = new StackLinkParser();
+        parser = new StackOverFlowLinkParser();
     }
 
     @Test
     public void linkParsingValidUrl() {
-        LinkInfo link = parser.linkParsing("https://stackoverflow.com/questions/1642028/what-is-the-operator-in-c");
+        LinkData link = parser.parseLink("https://stackoverflow.com/questions/1642028/what-is-the-operator-in-c");
         assertNotNull(link);
         assertEquals("1642028", link.toString());
     }
 
     @Test
     public void linkParsingInvalidUrl() {
-        LinkInfo link = parser.linkParsing("https://stackoverflow.com/search?q=unsupported%20link");
+        LinkData link = parser.parseLink("https://stackoverflow.com/search?q=unsupported%20link");
         assertNull(link);
     }
 }
