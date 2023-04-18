0ccc00ddaa5c2a52b71680b434518a28fce8144d
Shafikov Artur <lubetsiky02@mail.ru>
2023-04-18 19:26:47 +0500

    Fixed parsers tests

--- project/link-parser/src/main/test/ru/tinkoff/edu/java/LinkParser/Parsers/GitLinkParserTest.java
+++ project/link-parser/src/main/test/ru/tinkoff/edu/java/LinkParser/Parsers/GitLinkParserTest.java
@@ -2,31 +2,32 @@ package ru.tinkoff.edu.java.LinkParser.Parsers;
 
 import org.junit.Before;
 import org.junit.Test;
-import ru.tinkoff.edu.java.LinkParser.LinksInfo.LinkInfo;
+import ru.tinkoff.edu.java.LinkParser.data.LinkData;
+import ru.tinkoff.edu.java.LinkParser.parsers.GitHubLinkParser;
 
 import static org.junit.Assert.*;
 
 public class GitLinkParserTest {
 
-    GitLinkParser parser;
+    GitHubLinkParser parser;
 
     @Before
     public void setUp() throws Exception {
-        parser = new GitLinkParser();
+        parser = new GitHubLinkParser();
     }
 
     @Test
     public void linkParsingValidUrl() {
-        LinkInfo link = parser.linkParsing("https://github.com/sanyarnd/tinkoff-java-course-2022/");
+        LinkData link = parser.parseLink("https://github.com/sanyarnd/tinkoff-java-course-2022/");
         assertNotNull(link);
         assertEquals("sanyarnd/tinkoff-java-course-2022", link.toString());
     }
 
     @Test
     public void linkParsingInvalidUrl() {
-        LinkInfo link = null;
+        LinkData link = null;
         try {
-            link = parser.linkParsing("https://github.com/sanyarnd/");
+            link = parser.parseLink("https://github.com/sanyarnd/");
         } catch (Exception e) {
             System.err.println("Invalid URL");
         }
