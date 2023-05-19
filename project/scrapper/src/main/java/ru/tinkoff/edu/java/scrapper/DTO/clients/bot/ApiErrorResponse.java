3c46433850fb27e505559c10dccf6bc785826565
Shafikov Artur <lubetsiky02@mail.ru>
2023-05-19 21:42:21 +0500

    Created DTO for BotWebClient

---
+++ project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/DTO/clients/bot/ApiErrorResponse.java
@@ -0,0 +1,12 @@
+package ru.tinkoff.edu.java.scrapper.DTO.clients.bot;
+
+import java.util.List;
+
+public record ApiErrorResponse(
+    String description,
+    String code,
+    String exceptionName,
+    String exceptionMessage,
+    List<String> stacktrace
+) {
+}
